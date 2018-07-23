a574eef11d337e05cf96201e570b15d9756629df
JeonWihwan <38161763+JeonWihwan@users.noreply.github.com>
2018-07-23 04:48:48 +0000

    Create ASCII.cpp

---
+++ ASCII.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+ int num = 48;
+ char ch;
+ 
+ for(int i = 0; i < 10; i++)
+ {
+  ch = num;
+  cout << ch << endl;
+  num++;
+  } 
+ 
+ 
+ return 0;
+}
